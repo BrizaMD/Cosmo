e60ffc1c9a6e121f2e422cf76e47b22a7337a5d7
BrizaMD <late16@gmail.com>
2021-08-30 18:01:04 +0200

    [wip] tried to outsource fetch

---
+++ src/utility/Fetch.js
@@ -0,0 +1,16 @@
+// import React, {useState} from 'react';
+// import axios from 'react';
+//
+// function fetcher(url) {
+//     // const [data, setData] = useState({});
+//     // const [error, setError] = useState('');
+//     let data = {};
+//
+//     axios.get(url)
+//         .then(res => {
+//             data = res.data
+//         });
+//     return data;
+// }
+//
+// export default fetcher;
