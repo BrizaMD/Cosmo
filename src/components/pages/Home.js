d0129d53ed42e36d2528970617d99051c516d11d
BrizaMD <late16@gmail.com>
2021-08-30 18:01:26 +0200

    [added] pics to Home render and clickevent

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import fortuneCookie from '../../static/img/fortunecookie1.png';
+import brokenFortuneCookie from '../../static/img/fortunecookie2.png';
+import fetcher from "../../utility/Fetch";
+
 
 const Home = () => {
+    const url = 'https://api.adviceslip.com/advice';
+
+    const placeHolder = () => {
+        console.log(fetcher(url));
+    }
 
     return(
-        <div>Welcome home brother</div>
+        <div id={"fortune"}>
+            <img src={fortuneCookie} alt="fortune cookie" onClick={placeHolder}/>
+            <img src={brokenFortuneCookie} alt="broken fortune cookie" />
+        </div>
     )
 }
 
