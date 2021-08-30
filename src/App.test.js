1404afb0b67be0429b135f143843461cbfbfb1a9
MoniBohm <monic@MONCSI-DESKTOP>
2021-08-30 15:55:34 +0200

    [fixed] App.test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByAltText(/learn react/i);
+  const linkElement = screen.getByText(/Jurassic Park/i);
   expect(linkElement).toBeInTheDocument();
 });
