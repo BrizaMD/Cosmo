b5e2d4cd43e10a508057a46e33ba6e5e5fb96047
MoniBohm <monic@MONCSI-DESKTOP>
2021-08-31 11:11:24 +0200

    [added] stock video style elements

--- src/components/pages/Love.js
+++ src/components/pages/Love.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import styled from "styled-components";
+import {Link} from 'react-router-dom';
+import video from '../../static/video/Love - 3191.mp4';
 
 const Love = () => {
 
     return(
-        <div>I LOVE YOU</div>
+        <HeroContainer>
+            <video autoPlay loop muted  >
+                <source src={video} type="video/mp4"/>
+            </video>
+
+
+
+        </HeroContainer>
     )
 }
 
 export default Love;
+
+const HeroContainer = styled.div`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  box-shadow: inset 0 0 0 1000px rgba(0, 0, 0, 0.2);
+  object-fit: contain;
+  opacity: 0.5;
+
+  video{
+    object-fit: cover;
+    width: 100vw;
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    left: 0;
+  }
+`;
