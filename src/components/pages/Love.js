eac3c1748da3be20d56cae964e68b92982298d1e
MoniBohm <monic@MONCSI-DESKTOP>
2021-08-31 11:42:34 +0200

    [added] form elements

--- src/components/pages/Love.js
+++ src/components/pages/Love.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from "styled-components";
 import {Link} from 'react-router-dom';
 import video from '../../static/video/Love - 3191.mp4';
 
 const Love = () => {
 
-    return(
-        <HeroContainer>
-            <video autoPlay loop muted  >
-                <source src={video} type="video/mp4"/>
-            </video>
+    const [firstHuman, setFirstHuman] = useState('Your name');
+    const [secondHuman, setSecondHuman] = useState('Your partner name');
 
 
+    const myChangeHandler = (event) => {
+        this.setState({username: event.target.value});
+        setFirstHuman(event.target.value);
+    }
 
-        </HeroContainer>
+    return (
+        <>
+            <HeroContainer>
+                <video autoPlay loop muted>
+                    <source src={video} type="video/mp4"/>
+                </video>
+            </HeroContainer>
+            <FormContainer>
+                <form>
+                    <label> Love Compatibility</label>
+                    <input type="text" required value={firstHuman} onChange={myChangeHandler}/>
+                    <input type="text" required  value={secondHuman} onChange={myChangeHandler}/>
+                </form>
+                <button>Calculate it!</button>
+            </FormContainer>
+        </>
     )
 }
 
 export default Love;
 
 const HeroContainer = styled.div`
-  height: 100vh;
+  //height: 100vh;
   width: 100%;
   display: flex;
   flex-direction: column;
@@ -28,14 +44,46 @@ const HeroContainer = styled.div`
   align-items: center;
   box-shadow: inset 0 0 0 1000px rgba(0, 0, 0, 0.2);
   object-fit: contain;
-  opacity: 0.5;
+  opacity: 0.3;
 
-  video{
+  video {
     object-fit: cover;
     width: 100vw;
     height: 100vh;
     position: fixed;
     top: 0;
     left: 0;
+  }
+
+`;
+
+const FormContainer = styled.div`
+    max-width: 400px;
+    margin: 0 auto;
+    text-align: center ;
+
+    
+  label{
+    text-align: left;
+    display: block;
+  }
+  
+  input, textarea{
+    width:100%;
+    padding: 6px 10px;
+    margin: 10px 0;
+    border: 1px solid #ddd;
+    box-sizing: border-box;
+    display: block;
+  }
+  
+  button{
+    background: #f1356d;
+    color: white;
+    border: 0;
+    padding: 8px;
+    border-radius: 8px;
+    cursor: pointer;
+    
   }
 `;
