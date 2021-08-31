2b7e2e0d82532359eeb2dbcd1387f20b041bf35f
MoniBohm <monic@MONCSI-DESKTOP>
2021-08-31 12:53:47 +0200

    [created] form card

--- src/components/pages/Love.js
+++ src/components/pages/Love.js
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from 'react';
 import styled from "styled-components";
 import {Link} from 'react-router-dom';
 import video from '../../static/video/Love - 3191.mp4';
+import cover from '../../static/img/daisies.jpg'
+
 
 const Love = () => {
 
@@ -22,22 +24,58 @@ const Love = () => {
                 </video>
             </BackgroundContainer>
 
-            <FormContainer>
-                <form>
-                    <label> Love Compatibility</label>
 
-                    <input type="text" required value={firstHuman} onChange={myChangeHandler}/>
-                    <input type="text" required  value={secondHuman} onChange={myChangeHandler}/>
 
-                </form>
-                <button>Calculate it!</button>
-            </FormContainer>
+            <Card>
+                <img src={cover} alt="picture" />
+                <CardBody>
+
+                    <FormContainer>
+                        <form>
+                            <label> Love Compatibility</label>
+
+                            <input type="text" required value={firstHuman} onChange={myChangeHandler}/>
+                            <input type="text" required  value={secondHuman} onChange={myChangeHandler}/>
+
+                        </form>
+                        <button>Calculate it!</button>
+                    </FormContainer>
+
+                </CardBody>
+
+            </Card>
+
 
         </>
     )
 }
 
 export default Love;
+
+const Card = styled.div`
+    box-shadow: 0 0 20px 5px;
+    //border-radius: initial;
+    min-width: 28rem;
+    display: flex;
+    max-width: 400px;
+    margin: 0 auto;
+    text-align: center ;
+
+
+  
+  img{
+    width: 15rem;
+    height: 11rem;
+    object-fit: cover;
+    transform: scale(1.1);
+  }
+  
+`;
+
+const CardBody = styled.div`
+    margin:1rem;
+
+`;
 
 const BackgroundContainer = styled.div`
   width: 100%;
