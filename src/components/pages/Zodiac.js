9bcb0e6ab88aa728758c04ee85810412209f4208
BrizaMD <late16@gmail.com>
2021-08-31 11:18:23 +0200

    [added] base zodiac sign list

--- src/components/pages/Zodiac.js
+++ src/components/pages/Zodiac.js
@@ -1,39 +1,19 @@
 import React from 'react';
 import styled from "styled-components";
-import axios from "axios";
-import aquarius from '../../static/img/aquarius.jpg';
 
 const Zodiac = () => {
-    let signHolder = 'aquarius';
-    let buildUrl = 'https://aztro.sameerkumar.website/?sign=' + signHolder + '&day=today';
-
     const zodiacs = ['aquarius', 'pisces', 'aries', 'taurus', 'gemini', 'cancer', 'leo', 'virgo', 'libra', 'scorpio', 'sagittarius', 'capricorn'];
-
-
-
-    // const zodiac = async () => {
-    //     await axios.get(url)
-    //         .then(async res =>{
-    //             advice = await res.data.slip.advice;
-    //         })
-    //     console.log(advice);
-    // }
 
     return(
         <div>
             <SignContainer className={'signbuttons-container'}>
-                <div><img src={aquarius}  alt='Aquarius' /></div>
-                <div><img src={aquarius}  alt='Pisces' /></div>
-                <div><img src={aquarius}  alt='Aries' /></div>
-                <div><img src={aquarius}  alt='Taurus' /></div>
-                <div><img src={aquarius}  alt='Gemini' /></div>
-                <div><img src={aquarius}  alt='Cancer' /></div>
-                <div><img src={aquarius}  alt='Leo' /></div>
-                <div><img src={aquarius}  alt='Virgo' /></div>
-                <div><img src={aquarius}  alt='Libra' /></div>
-                <div><img src={aquarius}  alt='Scorpio' /></div>
-                <div><img src={aquarius}  alt='Sagittarius' /></div>
-                <div><img src={aquarius}  alt='Capricorn' /></div>
+                {zodiacs.map((sign) => (
+                    <div key={sign}>
+                        {sign}
+                        <img src={'zodiacs/' + sign + '.jpg'} alt={sign} />
+                    </div>
+                    )
+                )}
             </SignContainer>
 
             <SignDetailsContainer>
