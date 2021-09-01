01c67c392dbb969e21d24e67525f3136727421fb
MoniBohm <monic@MONCSI-DESKTOP>
2021-09-01 17:39:31 +0200

    [added] init test for cocktail page

--- src/components/__test__/Cocktail.test.js
+++ src/components/__test__/Cocktail.test.js
@@ -1,68 +1,23 @@
-// import React from "react";
-// import {act, fireEvent, getByText, render, screen} from '@testing-library/react';
-//
-// import ReactDOM, {unmountComponentAtNode} from 'react-dom';
-// import Cocktail from '../pages/Cocktail';
-//
-// let container = null;
-//
-// beforeEach(() => {
-//     // setup a DOM element as a render target
-//     container = document.createElement('div');
-//     document.body.appendChild(container);
-// });
-//
-// afterEach(() => {
-//     // cleanup on exiting
-//     unmountComponentAtNode(container);
-//     container.remove();
-//     container = null;
-// });
-//
-// test('renders withouth crashing', () => {
-//     const div = document.createElement("div");
-//     ReactDOM.render(<Cocktail />, div)
-// });
-//
-// test('renders drink from fetch', async() => {
-//     const div = document.createElement("div");
-//     ReactDOM.render(<Cocktail />, div);
-//
-//     fireEvent(
-//         getByText(div, 'Get a Drink!'),
-//         new MouseEvent('click', {
-//             bubbles: true,
-//             cancelable: true,
-//         }),
-//     );
-//
-//     const fakeDrink = {
-//         id: '1',
-//         name: 'The Coders Brew',
-//         category: 'Strongest',
-//         alcoholContent: 'Alcoholic',
-//         glassType: 'Huge glass',
-//         instructions_EN: 'You cannot simply make this drink, the drink will find you',
-//         picture: 'realpicture',
-//         recipeUrl: 'fakeurl.com'
-//     };
-//
-//     jest.spyOn(global, "fetch").mockImplementation(() =>
-//         Promise.resolve({
-//             json: () => Promise.resolve(fakeDrink)
-//         })
-//     );
-//
-//     await act( async () => {
-//         await render(<Cocktail />, container);
-//     })
-//
-//     expect(div.querySelector('div')).toBe(fakeDrink.name);
-//
-//     // expect(container.querySelector("summary").textContent).toBe(fakeUser.name);
-//     // expect(container.querySelector("strong").textContent).toBe(fakeUser.age);
-//     // expect(container.textContent).toContain(fakeUser.address);
-//
-//     // remove the mock to ensure tests are completely isolated
-//     global.fetch.mockRestore();
-// })
+import React from "react";
+import Cocktail from '../pages/Cocktail';
+import '@testing-library/jest-dom/extend-expect';
+import {render} from '@testing-library/react';
+
+
+beforeAll(() =>
+    Object.defineProperty(HTMLMediaElement.prototype, "muted", {
+        set: jest.fn(),
+    })
+);
+
+test('page has background-video, title and button', () =>{
+
+    const { getByTestId } = render(<Cocktail/>);
+    const background = getByTestId('background');
+    const btn = getByTestId('cocktail-button');
+    const title = getByTestId('title');
+
+    expect(background).toBeTruthy();
+    expect(btn).toBeTruthy();
+    expect(title).toBeTruthy();
+})
